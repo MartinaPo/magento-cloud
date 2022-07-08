c4d0584530e509419716a62b1844838818dd9db8
MartinaPo <koory@hotmail.it>
2022-07-08 14:28:18 +0000

    fix file grunt, aggiunta dev toolbar

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -50,5 +50,54 @@ module.exports = {
             'css/styles'
         ],
         dsl: 'less'
+    },
+
+    tematest_en: {
+        area: 'frontend',
+        name: 'Martina/Tematest',
+        locale: 'en_GB',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    tematest_it: {
+        area: 'frontend',
+        name: 'Martina/Tematest',
+        locale: 'it_IT',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    tematest_fr: {
+        area: 'frontend',
+        name: 'Martina/Tematest',
+        locale: 'fr_FR',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    tematest_de: {
+        area: 'frontend',
+        name: 'Martina/Tematest',
+        locale: 'de_DE',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
     }
 };
