7b446ac5c65e7371d425f70f9101cb1a61953dc8
MartinaPo <koory@hotmail.it>
2022-07-13 15:38:42 +0000

    fix addtocart

--- app/design/frontend/Martina/Tematest/requirejs-config.js
+++ app/design/frontend/Martina/Tematest/requirejs-config.js
@@ -2,6 +2,7 @@ var config = {
     map: {
         '*': {
             custom: 'Magento_Sales/js/custom',
+            'qty-counter': 'Magento_Catalog/js/qty-counter'
         },
         'Magento_Sales/js/custom': {
             select2: 'js/select2.min'
